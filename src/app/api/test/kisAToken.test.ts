ce7256829efb87c5cba2a8c824488cae2710a1c9
ocean <twyeo9006@gmail.com>
2025-06-05 16:34:09 +0900

    test : getToken ,  tokenCache 기능확인

--- src/app/api/test/kisAToken.test.ts
+++ src/app/api/test/kisAToken.test.ts
@@ -1,3 +1,45 @@
 import { getKisToken } from "@/app/api/kis/getToken";
 import * as tokenCache from "@/lib/tokenCache";
 import axios from "axios";
+
+jest.mock("axios");
+const mockedAxios = axios.post as jest.Mocked<typeof axios>;
+
+describe("getAccessToken", () => {
+  //**NOTE - 매 테스트마다 실행되는 함수
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.clearAllTimers();
+  }, 10);
+
+  test("if aToken is in cache, return aToken(접속토큰)", async () => {
+    //NOTE - getAccessToken을 돌렸을때 aToken이 리턴된다고 설정
+    jest.spyOn(tokenCache, "getAccessToken").mockReturnValue("aToken");
+    const aToken = await getKisToken();
+
+    expect(aToken).toBe("aToken");
+    expect(mockedAxios.post).not.toHaveBeenCalled();
+  });
+
+  test("if aToken is not in cache, 새로토큰 받기 후 저장", async () => {
+    jest.spyOn(tokenCache, "getAccessToken").mockReturnValue(null);
+
+    const mockedSetToken = jest.spyOn(tokenCache, "setAccessToken");
+
+    mockedAxios.post.mockReturnValue(
+      Promise.resolve({
+        data: {
+          access_token: "new-token",
+          token_type: "Bearer",
+          expires_in: 11111,
+          access_token_token_expired: "2025-06-10 12:00:00",
+        },
+      })
+    );
+
+    const aToken = await getKisToken();
+
+    expect(aToken).toBe("new-token");
+    expect(mockedSetToken).toHaveBeenCalled();
+  });
+});
